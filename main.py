6544a29ca20df2efd6e1d8d2ef49f8c6bb56fd66
Rafael Rossi <rafael.rossi@akerbp.com>
2021-09-21 15:04:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+from pandas import DataFrame
+'''
+# Test auto deploy streamlit app.
+'''
+
+f = st.slider(label='f:', min_value=1, max_value=10)
+x = np.linspace(0, 6, 1000)
+y = np.sin(f * x) 
+data = np.array([x, y]).transpose()
+df = DataFrame(data, columns=['x', 'y']).set_index('x')
+st.line_chart(data=df)
